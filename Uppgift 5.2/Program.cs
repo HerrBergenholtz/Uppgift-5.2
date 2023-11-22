81490408425a0bc50c893969c383de2f5444db4d
HerrBergenholtz <zbdv8800@gmail.com>
2023-11-22 10:01:21 +0100

    Add project files.

---
+++ Uppgift 5.2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Uppgift_5._2
+{
+    class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                int[] tal = new int[3];
+                tal[0] = 5; 
+                tal[1] = 6;
+                Console.WriteLine("Skriv in ett heltal");
+                tal[2] = int.Parse(Console.ReadLine());
+
+                foreach (int i in tal)
+                {
+                    Console.Write($"{i} ");
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Du gav fel input, pröva igen");
+                Main();
+            }
+        }
+    }
+}
